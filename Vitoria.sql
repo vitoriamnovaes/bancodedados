6a6ff050d80b12133d16546d2a5b2e957ba62cbf
Vitória Novaes <vitoria.m14@aluno.ifsc.edu.br>
2022-11-30 11:17:36 -0300

    arrumando script

--- Vitoria.sql
+++ Vitoria.sql
@@ -8,7 +8,7 @@ USE maratona;
 -- Table mydb`.`usuario`
 -- -----------------------------------------------------
 CREATE TABLE usuario (
-  idusuario INT NOT NULL,
+  idusuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nome_pessoa VARCHAR(45) NOT NULL,
   email VARCHAR(45) NOT NULL,
   data_cadastro DATETIME NOT NULL,
@@ -22,25 +22,19 @@ CREATE TABLE usuario (
 -- Table mydb`.`ator`
 -- -----------------------------------------------------
 CREATE TABLE ator (
-  idator INT NOT NULL,
+  idator INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nome_ator VARCHAR(45) NOT NULL,
-  data_nascimento_ator DATE NOT NULL,
-  sexo VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idator)
-
-
-);
+  data_nascimento_ator DATETIME NOT NULL,
+  sexo VARCHAR(45) NOT NULL
+ );
 -- -----------------------------------------------------
 -- Table mydb.series
 -- -----------------------------------------------------
 CREATE TABLE  series (
-  idseries INT NOT NULL,
+  idseries INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nome_serie VARCHAR(45) NOT NULL,
-  descricao VARCHAR(255) NOT NULL,
-  PRIMARY KEY (idseries)
-  
-
-);
+  descricao VARCHAR(255) NOT NULL
+  );
 
 -- -----------------------------------------------------
 -- Table mydb.serie_usuario
@@ -78,10 +72,8 @@ CREATE TABLE  elenco_serie (
 -- Table mydb.genero
 -- -----------------------------------------------------
 CREATE TABLE genero (
-  idgenero INT NOT NULL,
-  nome_genero VARCHAR(45) NOT NULL,
-  PRIMARY KEY (idgenero)
-  
+  idgenero INT NOT NULL  PRIMARY KEY AUTO_INCREMENT,
+  nome_genero VARCHAR(45) NOT NULL
 );
 
 
@@ -96,116 +88,76 @@ CREATE TABLE series_has_genero (
 -- inserts
 
 INSERT INTO usuario(
-  idusuario,
-  nome_pessoa,
+   nome_pessoa,
   email,
   data_cadastro,
   senha,
   data_nascimento
 )
-  VALUES (12341484980, 'Vitória Novaes', 'vitorianovaes@gmail.com', '16-08-06', '04-08-14'),
-         (55457899950, 'Sofia Nascimento', 'sofianascimento@gmail.com', '16-08-19', '09-06-04'),
-         (10586348956, 'Ana Silva', 'anasilva@gmail.com', '17-02-22','02-05-04'),
-         (29705500916, 'Sabrina Hostins', 'sabrinahostins@gmail.com', '14-09-18', '06-07-05'),
-         (11418350966, 'Milene Deschamps', 'milenedeschamps@gmail.com', '15-02-18', '02-07-19'),
-         (52456817907, 'Diego Augusto', 'diegoaugusto@gmail.com', '17-03-17', '06-08-02'),
-         (95099636900, 'Eloá Soares', 'eloasoares@gmail.com', '15-08-16', '02-08-04'),
-         (07165506950, 'Amélia Batista', 'ameliabatista@gmail.com', '16-09-18', '04-09-05'),
-         (67576429992, 'Amanda Zuchi', 'amandazuchi@gmail.com', '15-09-16', '09-05-04'),
-         (49191171911, 'Bernardo Schimitt', 'bernardoschimitt@gmail.com', '19-05-16', '08-08-04')
-;
-
-INSERT INTO serie_usuario(
-  status_serie,
-  nota_usuario
-)
-  VALUES ('assisti', 10),
-         ('quero assistir', 5),
-         ('assisti', 7),
-         ('quero assistir', 5),
-         ('assisti', 9),
-         ('assisti', 5),
-         ('quero assistir', 4),
-         ('assisti', 8),
-         ('quero assistir', 4),
-         ('assisti', 10)
+  VALUES ('Vitória Novaes', 'vitorianovaes@gmail.com', '2016-08-06', '1526', '2004-08-14'),
+         ('Sofia Nascimento', 'sofianascimento@gmail.com', '2016-08-19', '2151', '2009-06-04'),
+         ('Ana Silva', 'anasilva@gmail.com', '2017-02-22', '2156', '2002-05-04'),
+         ('Sabrina Hostins', 'sabrinahostins@gmail.com', '2014-09-18', '5421', '2006-07-05'),
+         ('Milene Deschamps', 'milenedeschamps@gmail.com', '2015-02-18', '1548', '2002-07-19'),
+         ('Diego Augusto', 'diegoaugusto@gmail.com', '2017-03-17', '3254', '2006-08-02'),
+         ('Eloá Soares', 'eloasoares@gmail.com', '2015-08-16', '787', '2002-08-04'),
+         ('Amélia Batista', 'ameliabatista@gmail.com', '2016-09-18', '7625','2004-09-05'),
+         ('Amanda Zuchi', 'amandazuchi@gmail.com', '2015-09-16', '5487', '2009-05-04'),
+         ('Bernardo Schimitt', 'bernardoschimitt@gmail.com', '2019-05-16', '4554', '2008-08-04')
 ;
 
 INSERT INTO series(
-  idserie,
   nome_serie,
   descricao
 )
-  VALUES (1, 'Gossip Girl', 'Favorita'),
-         (8, 'Bridgerton', 'Favorita'),
-         (2, 'Outer Banks', 'Amei'),
-         (5, 'Stranger things', 'Muito boa'),
-         (6, 'Coisa mais linda', 'Muito boa'),
-         (3, 'Virgin River', 'Amei'),
-         (9, 'Maldivas', 'Muito boa'),
-         (4, 'Emily in Paris', 'Favorita'),
-         (10, 'The 100', ' Muito boa'),
-         (7, 'Dark', 'Não gostei')
+  VALUES ('Gossip Girl', 'Favorita'),
+         ('Bridgerton', 'Favorita'),
+         ('Outer Banks', 'Amei'),
+         ('Stranger things', 'Muito boa'),
+         ('Coisa mais linda', 'Muito boa'),
+         ('Virgin River', 'Amei'),
+         ('Maldivas', 'Muito boa'),
+         ('Emily in Paris', 'Favorita'),
+         ('The 100', ' Muito boa'),
+         ('Dark', 'Não gostei')
 ;
 
 INSERT INTO genero(
-  idgenero, 
   nome_genero
 )
-  VALUES (1, 'Romance'),
-         (7, 'Romance'),
-         (9, 'Ficção cientifica'),
-         (5, 'Terror'),
-         (4, 'Ação'),
-         (3, 'Aventura'),
-         (2, 'Drama'), 
-         (6, 'Comédia'),
-         (10, 'Aventura'),
-         (8, 'terror')
-;
-
-INSERT INTO temporada_series(
-  idtemporada_series,
-  nr_episodios,
-  data_lancamento
-)
-  VALUES (2, '40', '18-02-18'),
-         (5, '55', '19-02-19'),
-         (10, '60', '12-08-12'),
-         (7, '102', '15-06-19'),
-         (4, '106', '18-09-17'),
-         (3, '18', '17-9-16'),
-         (1, '40', '18-07-19'), 
-         (6, '11', '18-02-08'),
-         (9, '27', '17-09-20'),
-         (8, '30', '14-02-17')
-
-
-;
+  VALUES ('Romance'),
+         ('Romance'),
+         ('Ficção cientifica'),
+         ('Terror'),
+         ('Ação'),
+         ('Aventura'),
+         ('Drama'), 
+         ('Comédia'),
+         ('Aventura'),
+         ('terror');
 
 INSERT INTO ator(
-  idator,
   nome_ator,
-  data_nascimento,
+  data_nascimento_ator,
   sexo
 )
-  VALUES (8, 'Leighton Meester', '1986-04-14', 'Feminino'),
-         (6, 'Phoebe Dynevor', '1995-04-17', 'Feminino'), 
-         (1, 'Madelyn Cline', '1997-12-14', 'Feminino'),
-         (3, 'Millie Bobby Brown', '2004-02-04', 'Feminino'),
-         (7, 'Maria Casadevall', '1987-07-19', 'Feminino'),
-         (2, 'Martin Henderson', '1974-10-07', 'Mascolino'),
-         (9, 'Bruna Marquezine', '1995-08-19', 'Feminino'),
-         (4, 'Lyli Colins', '1989-03-17', 'Feminino'),
-         (5, 'Bob Morley', '19840-12-04', 'Mascolino'),
-         (10, 'Louis Hofman', '1997-06-07', 'Mascolino')
+  VALUES ('Leighton Meester', '1986-04-14', 'Feminino'),
+         ('Phoebe Dynevor', '1995-04-17', 'Feminino'), 
+         ('Madelyn Cline', '1997-12-14', 'Feminino'),
+         ('Millie Bobby Brown', '2004-02-04', 'Feminino'),
+         ('Maria Casadevall', '1987-07-19', 'Feminino'),
+         ('Martin Henderson', '1974-10-07', 'Mascolino'),
+         ('Bruna Marquezine', '1995-08-19', 'Feminino'),
+         ('Lyli Colins', '1989-03-17', 'Feminino'),
+         ('Bob Morley', '1940-12-04', 'Mascolino'),
+         ('Louis Hofman', '1997-06-07', 'Mascolino')
 ;
 -- updates
-UPDATE usuario SET email = 'camilaandrade@gmail.com' WHERE idusuario LIKE 10586348956;
-UPDATE usuario SET email = 'anajulia@gmail.com' WHERE idusuario LIKE 49191171911;
+UPDATE usuario SET email = 'camilaandrade@gmail.com' WHERE idusuario = 1;
+UPDATE usuario SET email = 'anajulia@gmail.com' WHERE idusuario LIKE 491911;
 UPDATE usuario SET email = 'rayssa@gmail.com' WHERE idusuario LIKE 67576429992;
-UPDATE usuario SET email = 'larissaandrade@gmail.com' WHERE idusuario LIKE 95099636900;
-UPDATE usuario SET email = 'lorenzosouza@gmail.com' WHERE idusuario LIKE 11418350966;
+UPDATE usuario SET email = 'larissaandrade@gmail.com' WHERE idusuario LIKE 950996;
+UPDATE usuario SET email = 'lorenzosouza@gmail.com' WHERE idusuario LIKE 114183;
 
 
 UPDATE series SET nome_serie = 'Lúcifer' WHERE idserie LIKE 1;
@@ -233,11 +185,11 @@ UPDATE ator SET sexo = 'Feminino' WHERE idator LIKE 5;
 UPDATE ator SET sexo = 'Mascolino' WHERE idator LIKE 1;
 
 -- deletes
-DELETE FROM usuario WHERE idusuario = 10586348956;
-DELETE FROM usuario WHERE idusuario = 49191171911;
-DELETE FROM usuario WHERE idusuario = 67576429992;
-DELETE FROM usuario WHERE idusuario = 95099636900;
-DELETE FROM usuario WHERE idusuario = 11418350966;
+DELETE FROM usuario WHERE idusuario = 105863;
+DELETE FROM usuario WHERE idusuario = 491911;
+DELETE FROM usuario WHERE idusuario = 675764;
+DELETE FROM usuario WHERE idusuario = 950996;
+DELETE FROM usuario WHERE idusuario = 114183;
 
 DELETE FROM series WHERE idserie = 1;
 DELETE FROM series WHERE idserie = 2;
@@ -271,16 +223,16 @@ SELECT * FROM genero ORDER BY idgenero;
 SELECT * FROM temporada_serie ORDER BY idtemporada_serie;
 SELECT * FROM ator ORDER BY idator;
 
-SELECT * FROM usuario WHERE idusuario = 10586348956;
+SELECT * FROM usuario WHERE idusuario = 105863;
 SELECT * FROM series WHERE idserie = 2;
 SELECT * FROM genero WHERE idgenero = 5;
 SELECT * FROM temporada_serie WHERE idtemporada_serie = 7;
 SELECT * FROM ator WHERE idator = 3;
 
-SELECT usuario.idusuario, series.idserie FROM usuario RIGHT JOIN series ON usuario.idusuario * series.idseries;
-SELECT series.idseries, autor.idautor FROM series RIGHT JOIN autor ON series.idseries * autor.idautor;
-SELECT autor.idautor, temporada_serie.idtemporada_serie FROM autor RIGHT JOIN temporada_serie ON autor.idautor * temporada_serie.idtemporada_serie;
-SELECT temporada_serie.idtemporada_serie, genero.idgenero FROM temporada_serie RIGHT JOIN genero ON temporada_serie.idtemporada_serie * genero.idgenero;
-SELECT genero.idgenero, series.idserie FROM genero RIGHT JOIN series ON genero.idgenero * series.idserie;
+SELECT usuario.idusuario, series.idserie FROM usuario RIGHT JOIN series ON usuario.idusuario = series.idseries;
+SELECT series.idseries, autor.idautor FROM series RIGHT JOIN autor ON series.idseries = autor.idautor;
+SELECT autor.idautor, temporada_serie.idtemporada_serie FROM autor RIGHT JOIN temporada_serie ON autor.idautor = temporada_serie.idtemporada_serie;
+SELECT temporada_serie.idtemporada_serie, genero.idgenero FROM temporada_serie RIGHT JOIN genero ON temporada_serie.idtemporada_serie = genero.idgenero;
+SELECT genero.idgenero, series.idserie FROM genero RIGHT JOIN series ON genero.idgenero = series.idserie;
 
 -- DROP DATABASE maratona;
